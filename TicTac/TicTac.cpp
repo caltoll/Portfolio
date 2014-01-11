3f0073fdf969f3b455b807264c82ac7c1296ecbf
caltoll <gruntyhug@gmail.com>
2014-01-11 19:50:43 +0000

    Updated and cleaned design

--- TicTac/TicTac.cpp
+++ TicTac/TicTac.cpp
@@ -1,14 +1,187 @@
 //ticTac.cpp
-//game of tic tac toe from command line
+//game of tic tac toe from command line 
+//Restructrued to be class based
 
 #include <iostream>
 
 using namespace std;
 
-bool checkVictory(char board[3][3]); //Check if the board is in a victory state 											
-bool testArray(char array[3]); //As part of checkVictory: Test if an array of 3 characters are all equal
-void printBoard(char board[3][3]); // Print the board with characters separated by | and _
-bool makeMove(char board[3][3], char place);
+//Really I need to strip everything out
+//I can hack a class-based implementation together but really I should 
+//Take everything out 
+//Split into 2 files: For game and for board. Lets me use 
+
+//NEED
+//Board class
+
+//Game class? 
+
+class board
+{
+
+public:
+	board();
+	~board(){};
+
+	bool checkVictory(); //Check if the board is in a victory state & set getVictor											
+	void printBoard(); // Print the board with characters separated by | and _
+	bool makeMove(char place);
+	char getVictor(){return *victor;}
+	void resetBoard();
+
+private: 
+	bool testArray(char array[3]); //As part of checkVictory: Test if an array of 3 characters are all equal
+	char * victor; //Char which shows which made the winning move, set via pointer through checkVictory
+	char boardArray[3][3];
+};
+
+
+board::board()
+{
+	cout << "Board constructor call\n Initialising array with values: ";
+
+	for (int i = 0; i<10; i++)  
+		{
+			int row = i/3;
+			int collumn = i%3;
+			int val = i+1;
+			char cVal = '0' + val;
+			cout << cVal << " ";
+			boardArray[row][collumn] = cVal;
+		}
+	cout << "\nConstructor complete\n";
+}
+
+
+//=================================CHECKVICTORY=================================
+bool board::checkVictory()
+{
+	char testR[3];//TestRow
+	char testC[3];//TestCollumn
+	char testD1[3];
+	char testD2[3];
+	//loop rows
+	for (int i = 0; i<3; i++)
+	{
+		for (int j = 0; j<3; j++)
+		{
+			testR[j] = boardArray[i][j];
+			testC[j] = boardArray[j][i];
+
+		
+
+		}
+
+		testD1[i] = boardArray[i][i];
+		testD2[i] = boardArray[i][2-i];
+
+		if(board::testArray(testR))
+		{
+			cout<<"\tRow Complete"<<endl;
+			return true; 
+		}
+
+		if(board::testArray(testC))
+		{	
+			cout<<"\tColumn Complete"<<endl;
+			return true;
+		}
+
+		if(board::testArray(testD1))
+		{
+			cout << "\tDiagonal Complete" << endl;
+			return true;
+		}
+
+		if(board::testArray(testD2))
+		{
+			cout << "\tDiagonal Complete" << endl;
+			return true;
+		}
+	
+	}
+
+	
+	return false;
+}
+
+bool board::testArray(char array[3])
+{
+	if(array[0]==array[1] && array[1]==array[2])
+		return true;
+
+	return false;
+}
+
+bool board::makeMove(char piece)//Needs to change to take int, move logic to game methods
+{
+	int r,c;
+	int move;
+	if (!(cin >> move) || move > 9) //Check that its a valid integer
+	{
+		cout << "\nEnter a valid square to move to: ";
+	
+		cin.clear();
+		cin.ignore();
+		return false;
+
+	}
+	
+	move--; //Array measured from 0
+	r = move/3;
+	c = move%3;
+	if((boardArray[r][c] == 'X')||(boardArray[r][c] == 'O'))
+	{
+		cout << "\nThat space is already taken, enter another: ";
+		return false;
+	}
+	
+		//Int/3 always rounds down, 
+	boardArray[r][c] = piece;
+	printBoard();
+	return true;
+}
+
+//==============Print Board Function====================
+void board::printBoard()
+{
+	cout << "\n\n";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "\t\t";
+		for (int j = 0; j < 5; j++)
+		{
+			//EVEN rows & colums print i/2,j/2
+			if ( (i % 2 == 0) && (j % 2 == 0))
+				cout << boardArray[i/2][j/2];
+
+			//EVEN row & ODD colum print |
+			else if ((i % 2 == 0) && !(j % 2 == 0))
+				cout << " | ";
+
+			//ODD row & EVEN colum print _
+			else if (!(i % 2 == 0) && (j % 2 == 0))
+				cout << "__";
+
+			//ODD row & ODD colum print |_
+			else
+				cout << "|_";
+		}
+
+		cout <<" \n";
+	}
+}
+
+
+
+
+
+//Eventually move this to a separate file
+
+void turn()
+{
+
+}
 
 //=================================MAIN=================================
 int main()
@@ -24,7 +197,8 @@ int main()
 			cin.ignore();
 	while(true)
 	{	
-		char boardArray[3][3] = {'1','2','3','4','5','6','7','8','9'};
+		board gameBoard = board();
+		cout << "Back to main" << endl;
 		
 		char cont = '\0'; //Char will be set to y or n
 		char victory = '\0'; //Char will be set to X or O
@@ -32,7 +206,7 @@ int main()
 		int move;
 		int turn = 0;
 
-		printBoard(boardArray);
+		gameBoard.printBoard();
 		
 		
 		
@@ -42,10 +216,10 @@ int main()
 			//Nought's Turn
 			turn++;
 			cout << "\n\tNought: ";
-			while(!makeMove(boardArray, 'O'))
+			while(!gameBoard.makeMove( 'O'))
 			{}
 
-			if(checkVictory(boardArray))
+			if(gameBoard.checkVictory())
 			{
 				victory = 'O';
 				break;
@@ -57,10 +231,10 @@ int main()
 			//Cross' Turn
 			turn++;
 			cout << "\n\tCross: ";
-			while(!makeMove(boardArray, 'X'))
+			while(!gameBoard.makeMove('X'))
 			{}
 
-			if(checkVictory(boardArray))
+			if(gameBoard.checkVictory())
 			{
 				victory = 'X';
 				break;
@@ -97,125 +271,4 @@ int main()
 	
 }
 
-
-//=================================CHECKVICTORY=================================
-bool checkVictory(char board[3][3])
-{
-	char testR[3];//TestRow
-	char testC[3];//TestCollumn
-	char testD1[3];
-	char testD2[3];
-	//loop rows
-	for (int i = 0; i<3; i++)
-	{
-		for (int j = 0; j<3; j++)
-		{
-			testR[j] = board[i][j];
-			testC[j] = board[j][i];
-
-		
-
-		}
-
-		testD1[i] = board[i][i];
-		testD2[i] = board[i][2-i];
-
-		if(testArray(testR))
-		{
-			cout<<"\tRow Complete"<<endl;
-			return true; 
-		}
-
-		if(testArray(testC))
-		{	
-			cout<<"\tColumn Complete"<<endl;
-			return true;
-		}
-
-		if(testArray(testD1))
-		{
-			cout << "\tDiagonal Complete" << endl;
-			return true;
-		}
-
-		if(testArray(testD2))
-		{
-			cout << "\tDiagonal Complete" << endl;
-			return true;
-		}
-	
-	}
-
-	
-	return false;
-}
-
-bool testArray(char array[3])
-{
-	if(array[0]==array[1] && array[1]==array[2])
-		return true;
-
-	return false;
-}
-
-bool makeMove(char board[3][3], char piece)
-{
-	int r,c;
-	int move;
-	if (!(cin >> move) || move > 9) //Check that its a valid integer
-	{
-		cout << "\nEnter a valid square to move to: ";
-	
-		cin.clear();
-		cin.ignore();
-		return false;
-
-	}
-	 //Add error checking here!
-	//ensure move is int
-	//ensure array location has not already been taken
-	move--; //Array measured from 0
-	r = move/3;
-	c = move%3;
-	if((board[r][c] == 'X')||(board[r][c] == 'O'))
-	{
-		cout << "\nThat space is already taken, enter another: ";
-		return false;
-	}
-	
-		//Int/3 always rounds down, 
-	board[r][c] = piece;
-	printBoard(board);
-	return true;
-}
-
-//==============Print Board Function====================
-void printBoard(char board[3][3])
-{
-	cout << "\n\n";
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "\t\t";
-		for (int j = 0; j < 5; j++)
-		{
-			//EVEN rows & colums print i/2,j/2
-			if ( (i % 2 == 0) && (j % 2 == 0))
-				cout << board[i/2][j/2];
-
-			//EVEN row & ODD colum print |
-			else if ((i % 2 == 0) && !(j % 2 == 0))
-				cout << " | ";
-
-			//ODD row & EVEN colum print _
-			else if (!(i % 2 == 0) && (j % 2 == 0))
-				cout << "__";
-
-			//ODD row & ODD colum print |_
-			else
-				cout << "|_";
-		}
-
-		cout <<" \n";
-	}
-}
 
